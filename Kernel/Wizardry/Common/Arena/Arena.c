0944e9237864d75a1fd628cf28ccc39d08d81d50
JesterWizard <adapps101@gmail.com>
2025-11-27 23:23:12 +0000

    Feature - Show arena opponent in advance

--- Kernel/Wizardry/Common/Arena/Arena.c
+++ Kernel/Wizardry/Common/Arena/Arena.c
@@ -285,15 +285,37 @@ LYN_REPLACE_CHECK(ArenaUi_WagerGoldDialogue);
 void ArenaUi_WagerGoldDialogue(ProcPtr proc)
 {
     int multiplier = 1;
+    
 
 #if defined(SID_Ludopathy) && (COMMON_SKILL_VALID(SID_Ludopathy))
     if (SkillTester(gActiveUnit, SID_Ludopathy))
         multiplier = 2;
 #endif
 
+#ifdef CONFIG_SHOW_ARENA_OPPONENT_IN_ADVANCE
+    DrawUiFrame2(7, 9, 0x10, 8, 0);
+    SetTextFont(0);
+    InitSystemTextFont();
+
+    PutString(TILEMAP_LOCATED(gBG0TilemapBuffer, 8, 10), 0, GetStringFromIndex(gMid_Lv));
+    PutNumber(TILEMAP_LOCATED(gBG0TilemapBuffer, 12, 10), 2, gArenaState.opponentUnit->level);
+    PutString(TILEMAP_LOCATED(gBG0TilemapBuffer, 8, 12), 0, GetStringFromIndex(gArenaState.opponentUnit->pCharacterData->nameTextId));
+    PutString(TILEMAP_LOCATED(gBG0TilemapBuffer, 15, 10), 0, GetStringFromIndex(gArenaState.opponentUnit->pClassData->nameTextId));
+    PutString(TILEMAP_LOCATED(gBG0TilemapBuffer, 15, 12), 0, GetItemName(gArenaState.opponentWeapon));
+
+    if (gArenaState.playerPowerWeight - gArenaState.opponentPowerWeight >= 20)
+        PutString(TILEMAP_LOCATED(gBG0TilemapBuffer, 12, 14), TEXT_COLOR_SYSTEM_GREEN, "Good match");
+    else if (gArenaState.opponentPowerWeight - gArenaState.playerPowerWeight <= 20)
+        PutString(TILEMAP_LOCATED(gBG0TilemapBuffer, 12, 14), TEXT_COLOR_SYSTEM_BLUE, "Okay match");
+    else
+        PutString(TILEMAP_LOCATED(gBG0TilemapBuffer, 12, 14), TEXT_COLOR_SYSTEM_GRAY, "Bad match");
+
+#endif
+
     SetTalkNumber(ArenaGetMatchupGoldValue() * multiplier);
     StartArenaDialogue(0x8D2, proc);
     // TODO: msgid "Would you like to wager[.][NL][G] gold?[Yes]"
+
     return;
 }
 
