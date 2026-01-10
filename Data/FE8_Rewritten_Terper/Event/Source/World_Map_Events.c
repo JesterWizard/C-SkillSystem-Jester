98d99ba3f37a25d8608af6255c0832205c0d4058
JesterWizard <adapps101@gmail.com>
2026-01-10 11:19:41 +0000

    Fix JesterWizard/C-SkillSystem-Jester#432

--- Data/FE8_Rewritten_Terper/Event/Source/World_Map_Events.c
+++ Data/FE8_Rewritten_Terper/Event/Source/World_Map_Events.c
@@ -1297,7 +1297,7 @@ const EventScr EventScrWM_Ch8_SET_NODE[] = {
     WM_CENTERCAMONLORD(WM_MU_0)
     WM_REMOVELOCATION(WM_NODE_Renvall1)
     WM_REMOVEPATH(WM_PATH_06)
-    WM_LOADLOCATION3(WM_NODE_Renvall2)
+    WM_LOADLOCATION2(WM_NODE_Renvall2) // Don't show the blue light effect
     WM_SETDESTINATION(WM_NODE_Renvall2)
     WM_DRAWPATH2(WM_PATH_07)
     WmEvtSetUnitOnNode(WM_MU_0, WM_NODE_Renvall2) // ENOSUPP in EAstdlib
