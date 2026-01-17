8a30867599758322f943939f0fc6aebe6393458b
JesterWizard <adapps101@gmail.com>
2026-01-17 09:18:06 +0000

    Fix JesterWizard/C-SkillSystem-Jester#455

--- Kernel/Wizardry/Misc/SkillEffects/MenuSkills/Rally.c
+++ Kernel/Wizardry/Misc/SkillEffects/MenuSkills/Rally.c
@@ -228,7 +228,10 @@ u8 Rally_OnSelected(struct MenuProc *menu, struct MenuItemProc *item)
 
 static void callback_anim(ProcPtr proc)
 {
-	StartLightRuneAnim(proc, gActiveUnit->xPos, gActiveUnit->yPos);
+	PlaySoundEffect(0x269);
+	Proc_StartBlocking(ProcScr_DanceringAnim, proc);
+
+    BG_SetPosition(BG_0, -SCREEN_TILE_IX(gActiveUnit->xPos - 1), -SCREEN_TILE_IX(gActiveUnit->yPos - 2));
 }
 
 static void callback_exec(ProcPtr proc)
