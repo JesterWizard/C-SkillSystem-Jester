a7fa681d728c549a17fd1bc26533ebb0bcfee6c4
JesterWizard <adapps101@gmail.com>
2026-03-21 16:36:24 +0000

    Fixed white screen on MyBoy

--- Kernel/Wizardry/Common/DebugKit/source/mgba.c
+++ Kernel/Wizardry/Common/DebugKit/source/mgba.c
@@ -6,6 +6,7 @@
 #include <string.h>
 
 #include "mgba.h"
+#include "configs/config-debug.h"
 
 #define REG_DEBUG_ENABLE ((vu16 *) 0x4FFF780)
 #define REG_DEBUG_FLAGS ((vu16 *) 0x4FFF700)
@@ -15,6 +16,11 @@ extern int mgba_print_level;
 
 void mgba_print(const char *buf)
 {
+	#if !CONFIG_MGBA_PRINTF_EN
+	(void) buf;
+	return;
+	#endif
+
 	strcpy(REG_DEBUG_STRING, buf);
 
 	mgba_print_level &= 0x7;
@@ -23,6 +29,11 @@ void mgba_print(const char *buf)
 
 void mgba_printf(int level, const char *ptr, ...)
 {
+	#if !CONFIG_MGBA_PRINTF_EN
+	(void) level;
+	(void) ptr;
+	return;
+	#else
 	va_list args;
 	char buf[0x100];
 
@@ -33,16 +44,25 @@ void mgba_printf(int level, const char *ptr, ...)
 
 	mgba_print_level = level;
 	mgba_print(buf);
+	#endif
 }
 
 bool mgba_open(void)
 {
+	#if !CONFIG_MGBA_PRINTF_EN
+	return false;
+	#else
 	mgba_print_level = MGBA_LOG_INFO;
 	*REG_DEBUG_ENABLE = 0xC0DE;
 	return *REG_DEBUG_ENABLE == 0x1DEA;
+	#endif
 }
 
 void mgba_close(void)
 {
+	#if !CONFIG_MGBA_PRINTF_EN
+	return;
+	#endif
+
 	*REG_DEBUG_ENABLE = 0;
 }
