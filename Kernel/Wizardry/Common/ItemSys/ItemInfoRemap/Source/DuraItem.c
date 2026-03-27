eb72c79ae0a6d6f052dcc7afc9987753c33d3a20
JesterWizard <adapps101@gmail.com>
2026-03-27 22:35:01 +0000

    Arms scroll fixed

--- Kernel/Wizardry/Common/ItemSys/ItemInfoRemap/Source/DuraItem.c
+++ Kernel/Wizardry/Common/ItemSys/ItemInfoRemap/Source/DuraItem.c
@@ -35,6 +35,14 @@ char *GetDuraItemName(int item)
 	if (IsSkillScrollItem(item))
 		return GetSkillScrollItemName(item);
 
+#ifdef CONFIG_ITEM_INDEX_ARMS_SCROLL
+	if (ITEM_INDEX(item) == CONFIG_ITEM_INDEX_ARMS_SCROLL) {
+		char *result = GetStringFromIndex(GetItemData(ITEM_INDEX(item))->nameTextId);
+		result = StrInsertTact();
+		return result;
+	}
+#endif
+
 	Errorf("Invalid dura-item: %x", item);
 	return NULL;
 }
@@ -43,6 +51,11 @@ int GetDuraItemDescId(int item)
 {
 	if (IsSkillScrollItem(item))
 		return GetSkillScrollItemDescId(item);
+
+#ifdef CONFIG_ITEM_INDEX_ARMS_SCROLL
+	if (ITEM_INDEX(item) == CONFIG_ITEM_INDEX_ARMS_SCROLL)
+		return GetItemData(ITEM_INDEX(item))->descTextId;
+#endif
 
 	Errorf("Invalid dura-item: %x", item);
 	return 0;
@@ -53,6 +66,11 @@ int GetDuraItemUseDescId(int item)
 	if (IsSkillScrollItem(item))
 		return GetSkillScrollItemUseDescId(item);
 
+#ifdef CONFIG_ITEM_INDEX_ARMS_SCROLL
+	if (ITEM_INDEX(item) == CONFIG_ITEM_INDEX_ARMS_SCROLL)
+		return GetItemData(ITEM_INDEX(item))->useDescTextId;
+#endif
+
 	Errorf("Invalid dura-item: %x", item);
 	return 0;
 }
@@ -61,6 +79,11 @@ int GetDuraItemIconId(int item)
 {
 	if (IsSkillScrollItem(item))
 		return GetSkillScrollItemIconId(item);
+
+#ifdef CONFIG_ITEM_INDEX_ARMS_SCROLL
+	if (ITEM_INDEX(item) == CONFIG_ITEM_INDEX_ARMS_SCROLL)
+		return GetItemData(ITEM_INDEX(item))->iconId;
+#endif
 
 	Errorf("Invalid dura-item: %x", item);
 	return 0;
