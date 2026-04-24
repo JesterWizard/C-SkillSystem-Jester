f5fbdb805c5764d38370c0d376f5958a8a358d42
JesterWizard <adapps101@gmail.com>
2026-04-24 21:43:45 +0100

    Moved the thought bubble gfx

--- Kernel/Wizardry/Misc/WorldMapThoughts/WorldMapThoughts.c
+++ Kernel/Wizardry/Misc/WorldMapThoughts/WorldMapThoughts.c
@@ -286,7 +286,7 @@ static void WorldMapThoughtBubble_Init(struct MenuProc * menuProc)
         return;
 
     Decompress(bubbleEntry->bubble, gGenericBuffer);
-    Copy2dChr(gGenericBuffer, (void *)0x6015880, 16, 8);
+    Copy2dChr(gGenericBuffer, (void *)0x6016000, 16, 8);
 
 }
 
@@ -295,8 +295,8 @@ static void WorldMapThoughtBubble_Loop(struct MenuProc * menuProc)
     if (GetWorldMapThoughtBubbleForUnit(gPlaySt.chapterIndex, gGMData.units[0].id) == NULL)
         return;
 
-    PutSprite(4, 10, 10, gObject_64x64, TILEREF(0x2C4, 0x0));
-    PutSprite(4, 74, 10, gObject_64x64, TILEREF(0x2CC, 0x0));
+    PutSprite(4, 10, 10, gObject_64x64, TILEREF(0x300, 0x0));
+    PutSprite(4, 74, 10, gObject_64x64, TILEREF(0x308, 0x0));
 }
 
 struct ProcCmd const sProcScr_WorldMapThoughtBubble[] = {
