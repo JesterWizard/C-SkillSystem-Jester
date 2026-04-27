be8c772f2899ea9cbd9972eb9ad4e205e1d6049d
JesterWizard <adapps101@gmail.com>
2026-04-27 23:43:50 +0100

    Fix JesterWizard/C-SkillSystem-Jester#527

--- Kernel/Wizardry/Misc/MirrorMapSprites/MirrorSprites.c
+++ Kernel/Wizardry/Misc/MirrorMapSprites/MirrorSprites.c
@@ -1,6 +1,7 @@
 #include "gbafe.h"
 #include "kernel-lib.h"
 #include "common-chax.h"
+#include "scene.h"
 
 extern int gSMSSyncFlag;
 extern UnitIconWait unit_icon_wait_table[];
@@ -102,9 +103,18 @@ static int GetSpinTrapSpriteId(const struct Trap *trap)
 
 static void ReloadCustomTrapSpritePalettes(void)
 {
+    ApplyPalette(Pal_Spin_Tile, 0x10 + SPIN_TRAP_OBJ_PAL);
     ApplyPalette(Pal_Grass_Tile, 0x10 + GRASS_TRAP_OBJ_PAL);
     ApplyPalette(Pal_Boulder_Tile, 0x10 + BOULDER_TRAP_OBJ_PAL);
-    ApplyPalette(Pal_Spin_Tile, 0x10 + SPIN_TRAP_OBJ_PAL);
+}
+
+static void ReloadCustomTrapSpritePalettes(void)
+{
+    // Do not reload these palettes if we have characters talking, as they need those palette banks as well
+    if (IsTalkActive())
+        return;
+
+    ReloadCustomTrapSpritePalettes();
 }
 
 static struct SMSHandle *AddTrapSprite(int xDisplay, int yDisplay)
@@ -321,7 +331,7 @@ void PutUnitSpritesOam(void)
 {
     struct SMSHandle * it = gSMSHandleArray->pNext;
 
-    ReloadCustomTrapSpritePalettes();
+    ReloadCustomTrapSpritePalettesIfNotTalking();
 
     PutUnitSpriteIconsOam();
 
